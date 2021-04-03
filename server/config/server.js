830805850429d21f6e431a728d8c9cf4a52e52a0
NameerWaqas <nameerwaqas321@gmail.com>
2021-04-03 23:29:24 +0500

    update: setup strapi for server-side development.

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db7e709387c17f663799c49cd0d957fb'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1df25e87-b5c3-4d4b-ab33-6ccf06536657'
+};
