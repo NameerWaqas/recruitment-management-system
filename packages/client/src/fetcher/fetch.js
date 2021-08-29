bb717a5082bbbb0d8b4a30d1d4b643029d003427
nameerwaqas <nameerwaqas321@gmail.com>
2021-08-29 23:21:35 +0500

    refactor: deleted packages directory

--- packages/client/src/fetcher/fetch.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const privateAPI = axios.create({ baseURL: "http://40.71.41.2" });
-export const attachToken = (jwt) => {
-  privateAPI.defaults.headers.common.Authorization = `Bearer ${jwt}`;
-};
--- packages/client/src/redux/store.jsx
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import AuthReducer from "./reducers/authReducer";
-import OpeningReducer from "./reducers/newOpening";
-
-export default configureStore({
-  reducer: { auth: AuthReducer, newOpening: OpeningReducer },
-});
